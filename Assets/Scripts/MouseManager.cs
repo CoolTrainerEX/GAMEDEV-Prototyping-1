8631455d5cf92417fa47bfdcee2b9a478f359935
CoolTrainerEX <CoolTrainerEX@gmail.com>
2026-09-18 15:07:33 +0800

    final

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
